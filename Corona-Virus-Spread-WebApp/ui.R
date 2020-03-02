db06ab828c5e4b9c6087afe86e4cb92593564495
anishsingh20 <loveeanish@gmail.com>
2020-03-02 19:07:19 +0530

    starting to develop the UI of the application

--- Corona-Virus-Spread-WebApp/ui.R
+++ Corona-Virus-Spread-WebApp/ui.R
@@ -8,26 +8,40 @@
 #
 
 library(shiny)
+require(highcharter)
+require(dplyr)
+require(tidyr)
+require(shiny)
+require(shinydashboard)
+require(data.table)
+
+#reading the dataset and doing some modifications
+
+nCOV<-read_csv("data/2019_nCoV_data.csv",col_names = TRUE)
+nCOV$Date<-anydate(nCOV$Date)
+#ommitting any NA values if there
+nCOV<-na.omit(nCOV)
+
+#chaiging the column name
+colnames(nCOV)[3]="State"
+
+#attaching the data frame
+attach(nCOV)
+
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Analyzing COVID-19 Spread worldwide"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+        mainPanel("This dataset contains data from 22 JAN 2020 till 9 FEB 2020.") , 
+    ), #end sidebarlayout
+    
+    
+    
+)) #end shinUI
