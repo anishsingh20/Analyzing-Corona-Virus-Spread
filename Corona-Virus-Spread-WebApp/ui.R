cf7d7f571414596eae0892ad85ce976f9203d988
anishsingh20 <loveeanish@gmail.com>
2020-03-02 19:21:42 +0530

    made the skelaton of the APP

--- Corona-Virus-Spread-WebApp/ui.R
+++ Corona-Virus-Spread-WebApp/ui.R
@@ -40,13 +40,38 @@ dashboardPage(
     dashboardSidebar(
         sidebarMenu(
             
-        ) #end sidebarmenu
+            menuItem("Main Menu", tabName = "tab1" ,icon=icon("dashboard")),
+            menuItem("Major Countries affected", tabName = "tab12")
+            
+        ), #end sidebarmenu
         
         
         
-    ) # end dashboardsidebar
+    ), # end dashboardsidebar
     
-    
+    #dashboardBody
+    dashboardBody(
+        
+        #adding all the tabs
+        tabItems(
+            
+            tabItem(tabName ="tab1",
+                    
+                    h2("Analysing Corona Virus Spread from 22 Jan 2020 till 9th Feb 2020.",align="center",style="margin-top:-5px;"),
+                    br()
+                
+                
+            ),
+            
+            tabItem(tabName = "tab2",
+                    
+                    h3("Top Most countries Affected with most COVID-19 cases",align="center") ,
+                
+            )
+            
+        )
+        
+    )#end dashboardBody
     
     
     
