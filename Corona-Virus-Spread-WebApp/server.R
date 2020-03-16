a587a2cd265e83134917d49904b443cd4f563b1d
anishsingh20 <loveeanish@gmail.com>
2020-03-16 16:25:33 +0530

    making the app and reading the data

--- Corona-Virus-Spread-WebApp/server.R
+++ Corona-Virus-Spread-WebApp/server.R
@@ -7,6 +7,29 @@
 #    http://shiny.rstudio.com/
 #
 
+
+#reading the dataset and doing some modifications
+
+urlConfirmed <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+
+urlDeaths<-"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+
+urlRecoveries<-"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+
+latestConf<-read_csv(url(urlConfirmed))
+
+latestDeaths<-read_csv(url(urlDeaths))
+
+latestRecoveries<-read_csv(url(urlRecoveries))
+
+latestConf_long<- gather(latestConf, Date, Count, `1/22/20`:ncol(latestConf))
+
+latestDeaths_long<- gather(latestDeaths, Date, Count, `1/22/20`:ncol(latestDeaths) )
+
+latestRecoveries_long<- gather(latestRecoveries, Date, Count, `1/22/20`:ncol(latestRecoveries) )
+
+
+
 library(shiny)
 
 # Define server logic required to draw a histogram
